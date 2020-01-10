e179790ba48aae35e3d379a9d5ecac9dee2a1766
JieqingShi <jieqingshi@googlemail.com>
2020-01-10 16:13:12 +0100

    Implemented partial waypoint_updater

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,19 @@
 FROM ros:kinetic-robot
 LABEL maintainer="olala7846@gmail.com"
 
+# Adding proxy to resolve error
+#ENV http_proxy "http://q454024:guccibugattipoincarepug@proxy.muc:8081"
+#ENV https_proxy "http://q454024:guccibugattipoincarepug@proxy.muc:8080"
+
 # Install Dataspeed DBW https://goo.gl/KFSYi1 from binary
 # adding Dataspeed server to apt
 RUN sh -c 'echo "deb [ arch=amd64 ] http://packages.dataspeedinc.com/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-dataspeed-public.list'
 RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FF6D3CDA
 RUN apt-get update
-
 # setup rosdep
+ADD resolv.conf /etc/resolv.conf.override
+CMD cp /etc/resolv.conf.override /etc/resolv.conf
+RUN cat /etc/resolv.conf
 RUN sh -c 'echo "yaml http://packages.dataspeedinc.com/ros/ros-public-'$ROS_DISTRO'.yaml '$ROS_DISTRO'" > /etc/ros/rosdep/sources.list.d/30-dataspeed-public-'$ROS_DISTRO'.list'
 RUN rosdep update
 RUN apt-get install -y ros-$ROS_DISTRO-dbw-mkz
--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,7 +3,8 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
-
+from scipy.spatial import KDTree
+import numpy as np
 import math
 
 '''
@@ -21,7 +22,8 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+# LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 75
 
 
 class WaypointUpdater(object):
@@ -37,16 +39,59 @@ class WaypointUpdater(object):
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.base_waypoints = None
+        self.waypoints_2d = None
+        self.waypoint_tree = None
+        self.pose = None
 
-        rospy.spin()
+        # rospy.spin()
+        self.loop()
+
+    def loop(self):
+        rate = rospy.Rate(50)  # can be set lower, e.g. to 30 Hertz or less
+        while not rospy.is_shutdown():
+            if self.pose and self.base_waypoints:  # if pose and base_waypoints are initialized
+                # Get closest waypoint
+                closest_waypoint_idx = self.get_closest_waypoint_idx()
+                self.publish_waypoints(closest_waypoint_idx)
+            rate.sleep()
+
+    def get_closest_waypoint_idx(self):
+        x = self.pose.pose.position.x  # look in message definitions to check attributes
+        y = self.pose.pose.position.y
+        closest_idx = self.waypoint_tree.query([x, y], 1)[1]  # get closest index from KDtree
+
+        # check if closest idx corresponds to the waypoint ahead or behind the vehicle
+        closest_coord = self.waypoints_2d[closest_idx]
+        prev_coord = self.waypoints_2d[closest_idx - 1]
+
+        # use dot product for vectors constructed by the coordinates to check
+        cl_vect = np.array(closest_coord)
+        prev_vect = np.array(prev_coord)
+        pos_vect = np.array([x, y])
+
+        val = np.dot(cl_vect - prev_vect, pos_vect - cl_vect)
+
+        if val > 0:
+            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
+        return closest_idx
+
+
+    def publish_waypoints(self, closest_idx):
+        lane = Lane()
+        lane.header = self.base_waypoints.header
+        lane.waypoints = self.base_waypoints.waypoints[closest_idx:closest_idx + LOOKAHEAD_WPS]  # publish all waypoints which are ahead of car
+        self.final_waypoints_pub.publish(lane)
 
     def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+        self.pose = msg
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+        self.base_waypoints = waypoints  # make sure attribute is initialized before subscriber
+        if not self.waypoints_2d:
+            # construct waypoints as 2d coordinates
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
+            self.waypoint_tree = KDTree(self.waypoints_2d)
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
